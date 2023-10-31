65693a41b8ce1311baf6cbe37e40cdb6a03e71c0
Cristian Musat <cristutz00@gmail.com>
2023-10-31 11:25:10 +0200

    initial commit
    Signed-off-by: Cristian Musat <cristutz00@gmail.com>

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Berkshire_Swash, Cabin_Condensed } from "next/font/google";
+
+export const displayFont = Berkshire_Swash({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const lusitana = Cabin_Condensed({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
